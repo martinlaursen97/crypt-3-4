8994fb38518ec311e76fc7df32b0cd14b568e50f
martinlaursen97 <martin_laursen21@hotmail.com>
2022-10-10 16:12:05 +0200

    better solution with itertools

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from passlib.hash import sha512_crypt
+from itertools import product
 
 
-# pw 000-999
-for i in range(1000):
-    pw = str(i)
-    if i < 10:
-        pw = '00' + pw
-    elif i < 100:
-        pw = '0' + pw
+def find_password(iterable: str, size: int, salt: str, hash: str, rounds: int) -> str:
+    for i in product(iterable, repeat=size):
+        password = ''.join(i)
 
-    hash = sha512_crypt.using(salt='penguins', rounds=5000).hash(pw)
-    if hash == '$6$penguins$eP.EvNiF2A.MmRVWNgGj5WSXKK8DAf7oeK8/kkbollee.F0T4KAy.QEgNAX.6wLQY1XHmSID/5VkeFiEaSA2b0':
-        print(f'Password: {pw}')
-        break
+        if sha512_crypt.using(salt=salt, rounds=rounds).hash(password) == hash:
+            return password
+    return 'Not found'
+
+
+if __name__ == '__main__':
+    hash = '$6$penguins$eP.EvNiF2A.MmRVWNgGj5WSXKK8DAf7oeK8/kkbollee.F0T4KAy.QEgNAX.6wLQY1XHmSID/5VkeFiEaSA2b0'
+
+    pw = find_password("0123456789",
+                       3,
+                       'penguins',
+                       hash,
+                       5000)
+
+    print(f'Password: {pw}')
