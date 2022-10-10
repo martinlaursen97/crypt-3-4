095c4b86b446dc0449322b1a739befdb39975c80
martinlaursen97 <martin_laursen21@hotmail.com>
2022-10-10 16:16:26 +0200

    .

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ def find_password(iterable: str, size: int, salt: str, hash: str, rounds: int) -
 if __name__ == '__main__':
     hash = '$6$penguins$eP.EvNiF2A.MmRVWNgGj5WSXKK8DAf7oeK8/kkbollee.F0T4KAy.QEgNAX.6wLQY1XHmSID/5VkeFiEaSA2b0'
 
-    pw = find_password("0123456789",
-                       3,
-                       'penguins',
-                       hash,
-                       5000)
+    password = find_password(iterable="0123456789",
+                             size=3,
+                             salt='penguins',
+                             hash=hash,
+                             rounds=5000)
 
-    print(f'Password: {pw}')
+    print(f'Password: {password}')
